61beb2f7c8c0f031c452508352ecd31c721031cd
raulromerap <raul.romera@iesnervion.com>
2024-02-23 14:40:48 +0100

    23/02/2024

---
+++ EjerciciosClases/src/clases/CuentaCorriente.java
@@ -0,0 +1,58 @@
+package clases;
+
+public class CuentaCorriente {
+
+	private String dni;
+
+	private String nombre;
+
+	private double saldo;
+
+	public CuentaCorriente(String dni, double saldo) {
+
+		if(dni != null && dni != "" && saldo > 0) {
+		this.dni = dni;
+		this.saldo = saldo;
+		}
+
+	}
+
+	public CuentaCorriente(String dni, String nombre, double saldo) {
+
+		if(dni != null && dni != "" && saldo > 0 && nombre != null && nombre != "") {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.saldo = saldo;
+		}
+
+	}
+	
+	public boolean sacarDinero(double sacar) {
+		
+		boolean posible = false;
+		
+		if(saldo - sacar > 0) {
+			posible = true;
+			
+			saldo -= sacar;
+		}
+		
+		
+		return posible;
+	}
+
+	public void ingresarDinero(double ingreso) {
+		
+		saldo += ingreso;
+		
+	}
+	
+	public void mostrarInformacion(boolean muestraInfo) {
+		
+		if(muestraInfo) {
+			System.out.println("Nombre titular: " + nombre);
+			System.out.println("Saldo: " + saldo);
+		}
+		
+	}
+}
